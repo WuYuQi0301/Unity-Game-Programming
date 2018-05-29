7e1315b9bea9a2457f76606ae67913e33871d7c5
wuyuqi <270738671@qq.com>
2018-05-29 19:42:55 +0800

    paticle

---
+++ Assets/Mood1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mood1 : MonoBehaviour {
+
+    public float engineRevs;
+    public float exhaustRate;
+
+    ParticleSystem exhaust;
+
+    float time;
+
+    void Start()
+    {
+        exhaust = GetComponent<ParticleSystem>();
+        time = 0f;
+    }
+
+
+    void Update()
+    {
+        exhaust.
+        time += Time.deltaTime;
+        exhaust.emissionRate = engineRevs * exhaustRate;
+        if (time == 5f)
+        {
+            exhaust.enableEmission = false;
+        }
+    }
+}
