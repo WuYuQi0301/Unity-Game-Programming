b81238feccef2c61567e99dfbb73a4897f1bec28
wuyuqi <270738671@qq.com>
2018-06-05 22:28:17 +0800

    v1

---
+++ Assets/DragItem.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.UIElements;
+
+public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform m_tranform;
+    RectTransform m_rectTransform;
+    Vector3 previousPosition;
+    CanvasGroup canvasGroup;
+    GameObject previousGamebject;
+    Texture previousImage;
+
+    // Use this for initialization
+    void Start () {
+        m_tranform = this.gameObject.transform;
+        m_rectTransform = this.transform as RectTransform;
+        canvasGroup = GetComponent<CanvasGroup>();
+        previousPosition = m_tranform.position;
+        Debug.Log("start");
+        Debug.Log(gameObject.name);
+	}
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        try
+        {
+            /*让event trigger忽略自身，
+             *这样才可以让event trigger检测到它下面一层的对象,如包裹或物品格子等
+             */
+            canvasGroup.blocksRaycasts = false;
+            previousGamebject = eventData.pointerEnter;
+            Debug.Log(previousGamebject);
+            previousPosition = m_tranform.position;
+            /*
+             * 保证当前操作的对象能够优先渲染
+             * 即不会被其它对象遮挡住
+             */
+            gameObject.transform.SetAsLastSibling();
+        }
+        catch (Exception e) {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        try
+        {
+            Vector2 mousePos;
+            if(RectTransformUtility.ScreenPointToLocalPointInRectangle(m_rectTransform, eventData.position, eventData.pressEventCamera, out mousePos))
+            {
+                m_rectTransform.position = mousePos;
+            }
+            GameObject currentGameObject = eventData.pointerEnter;
+
+            bool isin = IsInContainer(currentGameObject);
+            if(isin == true)
+            {
+                Image img = currentGameObject.GetComponent<Image>();
+                previousGamebject.GetComponent<Image>().image = previousImage;
+                if (previousGamebject != currentGameObject)
+                {
+                    previousGamebject.GetComponent<Image>().image = previousImage;
+                    previousGamebject = currentGameObject;//记录当前物品格子以供下一帧调用
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        try
+        {
+            GameObject currentContainer = eventData.pointerEnter;
+            if(currentContainer == null)
+            {
+                m_tranform.position = previousPosition;
+            }
+            else if(currentContainer.name == "Container")
+            {
+                m_tranform.position = currentContainer.transform.position;
+                previousPosition = m_tranform.position;
+                currentContainer.GetComponent<Image>().image = previousImage;//当前格子恢复正常颜色
+            }
+            else if(currentContainer.name == eventData.pointerDrag.name && currentContainer!= eventData.pointerDrag)
+            {
+                Vector3 temp = currentContainer.transform.position;
+                currentContainer.transform.position = previousPosition;
+                m_tranform.position = temp;
+                previousPosition = m_tranform.position;
+            }
+            else
+            {
+                m_tranform.position = previousPosition;
+            }
+            canvasGroup.blocksRaycasts = true;
+        }
+        catch (Exception e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    bool IsInContainer(GameObject gm)
+    {
+        if (gm == null)
+        {
+            return false;
+        }
+        return gm.name == "Container";
+    }
+}
---
+++ Assets/windows.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windows : MonoBehaviour {
+    Vector2 range = new Vector2(5f, 3f);
+
+    Transform m_trasform;
+    Quaternion m_quaternion;
+    Vector2 rot = Vector2.zero;
+	// Use this for initialization
+	void Start () {
+        m_trasform = this.gameObject.transform;
+        m_quaternion = m_trasform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = Input.mousePosition;
+        float screenWidth = Screen.width * 0.5f;
+        float screenHeight = Screen.height * 0.5f;
+        float x = Mathf.Clamp((pos.x - screenWidth) / screenWidth, -1f, 1f);
+        float y = Mathf.Clamp((pos.y - screenHeight)/screenHeight, -1f, 1f);
+        rot = Vector2.Lerp(rot, new Vector2(x, y), Time.deltaTime * 5f);
+        m_trasform.localRotation = m_quaternion * Quaternion.Euler(-rot.y * range.y, rot.x * range.x, 0);
+	}
+}
